ad2331ae0672210e1fad3164aefb3c3e36882346
mesinkasir <hockeycorpmarketing@gmail.com>
2025-10-08 19:26:41 +0700

    one page bootstrap 11ty

---
+++ content/index.md
@@ -0,0 +1,50 @@
+---
+header:
+  title: "GRAYSCALE"
+  description: "A free, responsive, one page Bootstrap theme created by Start Bootstrap."
+  video:  
+  image: /img/bg-masthead.jpg
+  button: "Get Started"
+  url: "#about"
+
+about:
+  section_name: "about"
+  title: "Built with Bootstrap 5"
+  content: |
+    Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on 
+    [the preview page](https://startbootstrap.com/theme/grayscale/).
+    The theme is open source, and you can use it for any purpose, personal or commercial.
+  video: 
+  image: "/img/ipad.png"
+
+section:
+  - section_name: "projects"
+    title: "Projects"
+    description: "Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!"
+    media: "/img/bg-masthead.jpg"
+    sub_section1:
+      - title: "Misty"
+        video: https://www.youtube.com/watch?v=JG7CjvB_eQc&pp=0gcJCfwJAYcqIYzv
+        image: 
+        content: "An example of where you can put an image of a project, or anything else, along with a description."
+    sub_section2:
+      - title: "Mountains"
+        video: 
+        image: "/img/demo-image-02.jpg"
+        content: "Another example of a project with its respective description."
+  - section_name: "services"
+    title: "Services"
+    description: "Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!"
+    media: "/img/bg-masthead.jpg"
+    sub_section1:
+      - title: "Services Misty"
+        video: 
+        image: "/img/demo-image-01.jpg"
+        content: "An example of where you can put an image of a project, or anything else, along with a description."
+    sub_section2:
+      - title: "Services Mountains"
+        video: 
+        image: "/img/demo-image-02.jpg"
+        content: "Another example of a project with its respective description."
+layout: home.njk
+---
---
+++ eleventy.config.js
@@ -0,0 +1,102 @@
+import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from "@11ty/eleventy";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import pluginNavigation from "@11ty/eleventy-navigation";
+import yaml from "js-yaml";
+import pluginFilters from "./_config/filters.js";
+import markdownIt from "markdown-it";
+import embedEverything from "eleventy-plugin-embed-everything";
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+export default async function(eleventyConfig) {
+	
+	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+			return false;
+		}
+	});
+
+	eleventyConfig
+		.addPassthroughCopy({
+			"./public/": "/"
+		})
+		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
+	eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+	eleventyConfig.addWatchTarget("css/**/*.css");
+	eleventyConfig.addPlugin(embedEverything);
+	eleventyConfig.addWatchTarget("content/**/*.{svg,webp,png,jpg,jpeg,gif}");
+	eleventyConfig.addBundle("css", {
+		toFileDirectory: "dist",
+		bundleHtmlContentFromSelector: "style",
+	});
+	eleventyConfig.addBundle("js", {
+		toFileDirectory: "dist",
+		bundleHtmlContentFromSelector: "script",
+	});
+	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
+		preAttributes: { tabindex: 0 }
+	});
+	eleventyConfig.addPlugin(pluginNavigation);
+	eleventyConfig.addPlugin(HtmlBasePlugin);
+	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom",
+		outputPath: "/feed/feed.xml",
+		stylesheet: "pretty-atom-feed.xsl",
+		templateData: {
+			eleventyNavigation: {
+				key: "Feed",
+				order: 4
+			}
+		},
+		collection: {
+			name: "posts",
+			limit: 10,
+		},
+		metadata: {
+			language: "en",
+			title: "Blog Title",
+			subtitle: "This is a longer description about your blog.",
+			base: "https://example.com/",
+			author: {
+				name: "Your Name"
+			}
+		}
+	});
+	  let markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+	let markdownLib = markdownIt(markdownItOptions);
+	eleventyConfig.addFilter('markdownify', (markdownString) => {
+		return markdownLib.render(markdownString);
+	});
+	eleventyConfig.addPlugin(pluginFilters);
+
+	eleventyConfig.addPlugin(IdAttributePlugin, {
+	});
+
+	eleventyConfig.addShortcode("currentBuildDate", () => {
+		return (new Date()).toISOString();
+	});
+};
+
+export const config = {
+	templateFormats: [
+		"md",
+		"njk",
+		"html",
+		"liquid",
+		"11ty.js",
+	],
+	markdownTemplateEngine: "njk",
+	htmlTemplateEngine: "njk",
+	dir: {
+		input: "content",          // default: "."
+		includes: "../_includes",  // default: "_includes" (`input` relative)
+		data: "../_data",          // default: "_data" (`input` relative)
+		output: "_site"
+	},
+
+};
